45ae25914315fe753746f2c98e4d72dfc462897c
RafiMangadavu <rafimangadavu2025@gmail.com>
2023-09-08 18:51:41 +0530

    initial commit

---
+++ resources/views/auth/forgot-password.blade.php
@@ -0,0 +1,162 @@
+ <!doctype html>
+<html lang="en">
+
+    <head>
+        
+        <meta charset="utf-8" />
+        <title>Recover Password | Admin</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Premium Multipurpose Admin & Dashboard Template" name="description" />
+        <meta content="Themesdesign" name="author" />
+        <!-- App favicon -->
+    <link rel="shortcut icon" href="{{ asset('backend/assets/images/favicon.ico')}}">
+
+    <!-- Bootstrap Css -->
+    <link href="{{ asset('backend/assets/css/bootstrap.min.css')}}" id="bootstrap-style" rel="stylesheet" type="text/css" />
+    <!-- Icons Css -->
+    <link href="{{ asset('backend/assets/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+    <!-- App Css-->
+    <link href="{{ asset('backend/assets/css/app.min.css')}}" id="app-style" rel="stylesheet" type="text/css" />
+
+
+    </head>
+
+    <body class="auth-body-bg">
+        <div class="bg-overlay"></div>
+        <div class="wrapper-page">
+            <div class="container-fluid p-0">
+                <div class="card">
+                    <div class="card-body">
+    
+                        <div class="text-center mt-4">
+                            <div class="mb-3">
+                                <a href="index.html" class="auth-logo">
+                                    <img src="{{ asset('backend/assets/images/logo-dark.png')}}" height="30" class="logo-dark mx-auto" alt="">
+                                    <img src="{{ asset('backend/assets/images/logo-light.png')}}" height="30" class="logo-light mx-auto" alt="">
+                                </a>
+                            </div>
+                        </div>
+    
+                        <h4 class="text-muted text-center font-size-18"><b>Reset Password</b></h4>
+    
+                        <div class="p-3">
+   
+        <form class="form-horizontal mt-3" method="POST" action="{{ route('password.email') }}">
+            @csrf
+
+        <div class="alert alert-info alert-dismissible fade show" role="alert">
+            Forgot your password? <strong>No problem.</strong> Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+
+        <div class="form-group mb-3">
+            <div class="col-xs-12">
+                <input class="form-control" id="email" type="email" name="" required="email" placeholder="Email">
+            </div>
+        </div>
+
+        <div class="form-group pb-2 text-center row mt-3">
+            <div class="col-12">
+                <button class="btn btn-info w-100 waves-effect waves-light" type="submit">Send Email</button>
+            </div>
+        </div>
+    </form>
+                        </div>
+                    </div>
+                    <!-- end cardbody -->
+                </div>
+                <!-- end card -->
+            </div>
+            <!-- end container -->
+        </div>
+        <!-- end -->
+        
+
+        <!-- JAVASCRIPT -->
+        <script src="{{ asset('backend/assets/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{ asset('backend/assets/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{ asset('backend/assets/libs/metismenu/metisMenu.min.js')}}"></script>
+        <script src="{{ asset('backend/assets/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{ asset('backend/assets/libs/node-waves/waves.min.js')}}"></script>
+
+        <script src="assets/js/app.js"></script>
+
+    </body>
+</html> 
+
+
+
+{{-- <x-guest-layout>
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('login') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="username" :value="__('Username')" />
+            <x-text-input id="username" class="block mt-1 w-full" type="text" name="username" :value="old('username')" required autofocus autocomplete="username" />
+            <x-input-error :messages="$errors->get('username')" class="mt-2" />
+        </div>
+
+        <!-- Password -->
+        <div class="mt-4">
+            <x-input-label for="password" :value="__('Password')" />
+
+            <x-text-input id="password" class="block mt-1 w-full"
+                            type="password"
+                            name="password"
+                            required autocomplete="current-password" />
+
+            <x-input-error :messages="$errors->get('password')" class="mt-2" />
+        </div>
+
+        <!-- Remember Me -->
+        <div class="block mt-4">
+            <label for="remember_me" class="inline-flex items-center">
+                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
+                <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            </label>
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            @if (Route::has('password.request'))
+                <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" href="{{ route('password.email')}}">
+                    {{ __('Forgot your password?') }}
+                </a>
+            @endif
+
+            <x-primary-button class="ml-3">
+                {{ __('Log in') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout> 
+
+
+ <x-guest-layout>
+    <div class="mb-4 text-sm text-gray-600">
+        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+    </div>
+
+    <!-- Session Status -->
+    <x-auth-session-status class="mb-4" :status="session('status')" />
+
+    <form method="POST" action="{{ route('password.email') }}">
+        @csrf
+
+        <!-- Email Address -->
+        <div>
+            <x-input-label for="email" :value="__('Email')" />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-input-error :messages="$errors->get('email')" class="mt-2" />
+        </div>
+
+        <div class="flex items-center justify-end mt-4">
+            <x-primary-button>
+                {{ __('Email Password Reset Link') }}
+            </x-primary-button>
+        </div>
+    </form>
+</x-guest-layout> --}}
---
+++ routes/web.php
@@ -0,0 +1,94 @@
+<?php
+
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Home\HomeSliderController;
+use App\Http\Controllers\Home\AboutController;
+use App\Http\Controllers\Home\PortfolioController;
+use App\Http\Controllers\Home\BlogCategoryController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('frontend.index');
+});
+
+Route::get('/dashboard', function () {
+    return view('admin.index');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+Route::middleware(['auth'])->group(function(){
+    Route::get('/admin/logout', [AdminController::class, 'destroy'])->name('admin.logout');
+    Route::get('/admin.profile', [AdminController::class, 'Profile'])->name('admin.profile');
+    Route::get('/edit.profile', [AdminController::class, 'EditProfile'])->name('edit.profile');
+    Route::get('/change/password', [AdminController::class, 'ChangePassword'])->name('change.password');
+    Route::post('/store.profile', [AdminController::class, 'StoreProfile'])->name('store.profile');
+    Route::post('/update/password', [AdminController::class, 'UpdatePassword'])->name('update.password');
+   
+});
+
+//Home Slide All Route
+Route::middleware(['auth'])->group(function(){
+    Route::get('/home/slide', [HomeSliderController::class, 'HomeSlider'])->name('home.slide');
+    Route::post('/update/slider', [HomeSliderController::class, 'UpdateSlider'])->name('update.slider');
+    
+   
+});
+//About Page All Route
+Route::middleware(['auth'])->group(function(){
+    Route::get('/about/page', [AboutController::class, 'AboutPage'])->name('about.page');
+    Route::post('/update/about', [AboutController::class, 'UpdateAbout'])->name('update.about');
+    Route::get('/about', [AboutController::class, 'HomeAbout'])->name('home.about');
+    Route::get('/about/multi/image', [AboutController::class, 'AboutMultiImage'])->name('about.multi.image');
+    Route::post('/store/multi/image', [AboutController::class, 'StoreMultiImage'])->name('store.multi.image');
+    Route::get('all/multi/image', [AboutController::class, 'AllMultiImage'])->name('all.multi.image');
+    Route::get('edit/multi/image/{id}', [AboutController::class, 'EditMultiImage'])->name('edit.multi.image');
+    
+    Route::post('/update/multi/image', [AboutController::class, 'UpdateMultiImage'])->name('update.multi.image');
+
+    Route::get('delete/multi/image/{id}', [AboutController::class, 'DeleteMultiImage'])->name('delete.multi.image');
+});
+
+// Portfolio All Route 
+Route::middleware('auth')->group(function () {
+    Route::get('/all/portfolio', [PortfolioController::class, 'AllPortfolio'])->name('all.portfolio');
+    Route::get('/add/portfolio', [PortfolioController::class, 'AddPortfolio'])->name('add.portfolio');
+    Route::post('/store/portfolio', [PortfolioController::class, 'StorePortfolio'])->name('store.portfolio');
+    Route::get('/edit/portfolio/{id}', [PortfolioController::class, 'EditPortfolio'])->name('edit.portfolio');
+    Route::post('/update/portfolio', [PortfolioController::class, 'UpdatePortfolio'])->name('update.portfolio');
+    Route::get('/delete/portfolio/{id}', [PortfolioController::class, 'DeletePortfolio'])->name('delete.portfolio');
+    Route::get('portfolio/details{id}', [PortfolioController::class, 'PortfolioDetails'])->name('portfolio.details');
+    
+});
+
+//BlogCategory All Route
+Route::middleware(['auth'])->group(function(){
+    Route::get('/all/blog/category', [BlogCategoryController::class, 'AllBlogCategory'])->name('all.blog.category');
+    Route::get('/add/blog/category', [BlogCategoryController::class, 'AddBlogCategory'])->name('add.blog.category');
+    Route::post('/store/blog/category', [BlogCategoryController::class, 'StoreBlogCategory'])->name('store.blog.category');
+    
+    
+   
+});
+
+
+
